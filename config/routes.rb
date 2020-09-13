906a2468ba4b41abb088c62b654e6f68481e4f6c
Maykell Sanchez Romero <kellsaro@gmail.com>
2020-09-13 17:14:00 -0500

    Versioning the api

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,15 @@
 Rails.application.routes.draw do
-  resources :authors
-  resources :book_items
-  resources :book_genres
-  resources :books
-  resources :categories
-  resources :genres
-  resources :libraries
+  namespace :api do
+    namespace :v1 do
+      resources :authors
+      resources :book_items
+      resources :book_genres
+      resources :books
+      resources :categories
+      resources :genres
+      resources :libraries
+    end
+  end
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
